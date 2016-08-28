bb4c14c13a759ba7318809db354ba39f1b510d38
Michael Pfurtscheller <pfu.mi@outlook.com>
2016-08-28 13:19:27 +0200

    Navigationsbar eingeführt, Reiter für GraphApi

---
+++ OnAPPoint/Controllers/GraphTestController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OnAPPoint.Controllers
+{
+  public class GraphTestController : Controller
+  {
+    // GET: GraphTest
+    public ActionResult Index()
+    {
+      // Provisorische Prüfung ob User eingeloggt
+      if (Session["AccessToken"] == null)
+      {
+        return new HttpStatusCodeResult(System.Net.HttpStatusCode.Unauthorized);
+      }
+      return View();
+    }
+  }
+}
