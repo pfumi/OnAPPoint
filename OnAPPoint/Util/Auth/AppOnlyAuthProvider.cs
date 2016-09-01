748f2b878ec88de92dd46999d91b2eb7a719e992
pfurma <matthias.pfurtscheller@pfurml.onmicrosoft.com>
2016-09-01 12:14:02 +0200

    MOD: Applikation läuft jetzt auf Azure :) und auch lokal

--- OnAPPoint/Util/Auth/AppOnlyAuthProvider.cs
+++ OnAPPoint/Util/Auth/AppOnlyAuthProvider.cs
@@ -39,6 +39,16 @@ namespace OnAPPoint.Util
             if (accessToken != null)
               return (accessToken);
 
+            X509Store certStore = new X509Store(StoreName.My, StoreLocation.CurrentUser);
+            certStore.Open(OpenFlags.ReadOnly);
+            X509Certificate2Collection certCol = certStore.Certificates.Find(
+              X509FindType.FindByThumbprint,
+              "38661500B0431DC2D7C73F02AE9FEC85D6479E95",
+              false
+              );
+            X509Certificate2 cert = certCol[0];
+
+      /*
             var certFile = System.IO.File.OpenRead(pfxFilePath);
             var certBytes = new byte[certFile.Length];
             certFile.Read(certBytes, 0, (int)certFile.Length);
@@ -49,6 +59,7 @@ namespace OnAPPoint.Util
                 X509KeyStorageFlags.MachineKeySet |
                 X509KeyStorageFlags.PersistKeySet
                 );
+      */
 
             ClientAssertionCertificate cac = new ClientAssertionCertificate(appId, cert);
             AuthenticationContext authContext = new AuthenticationContext("https://login.microsoftonline.com/" + tenant, false);
